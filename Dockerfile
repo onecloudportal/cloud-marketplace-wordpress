591ee396584dd97b32abe734c39bff83a144ea09
Saint Clair <saintclair.com@gmail.com>
2016-02-02 15:02:39 -0200

    Cloud Marketplace Wordpress

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM dell/lamp-base:1.2
+MAINTAINER Dell Cloud Market Place <Cloud_Marketplace@dell.com>
+
+# Download Wordpress 4.0 into /app
+RUN rm -fr /var/www/html
+RUN rm -fr /app && git clone -b 4.1-branch --single-branch --depth=1 \
+    https://github.com/WordPress/WordPress.git /app 
+
+# Configure Wordpress to connect to local DB
+COPY wp-config.php /app/wp-config.php
+
+# Add htaccess
+COPY .htaccess /app/.htaccess
+
+# Override Apache configuration
+COPY apache_default /etc/apache2/sites-available/000-default.conf
+
+# Add scripts and make them executable.
+COPY create_mysql_admin_user.sh /create_mysql_admin_user.sh
+COPY create_db.sh /create_db.sh
+COPY run.sh /run.sh
+RUN chmod +x /*.sh
+
+# Add volumes for MySQL and the application.
+VOLUME ["/var/lib/mysql", "/var/www/html"]
+
+EXPOSE 80 3306 443
+
+CMD ["/run.sh"]
---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VOLUME_HOME="/var/lib/mysql"
+APPLICATION_HOME="/var/www/html"
+
+sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+
+# Check if VOLUME_HOME has content
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+    echo "=> Installing MySQL ..."
+    mysql_install_db > /dev/null 2>&1
+    echo "=> Done!"  
+    /create_mysql_admin_user.sh
+else
+    echo "=> Using an existing volume of MySQL"
+fi
+
+# If the application directory is empty, copy the site.
+if [ ! "$(ls -A $APPLICATION_HOME)" ]; then
+    echo "=> Copying the site"
+
+    # Copy the application folder.
+    cp -r /app/. $APPLICATION_HOME
+
+    # Configure ownership to allow plugin upload.
+    chown -R www-data:www-data $APPLICATION_HOME/wp-content
+fi
+
+exec supervisord -n
